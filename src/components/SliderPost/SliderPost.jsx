8e6f02022231900dfdca5fee177dae8e8fd55ce1
hendrasob <mahendraagusti@gmail.com>
2024-12-23 10:00:16 +0700

    atur responsive slider post

--- src/components/SliderPost/SliderPost.jsx
+++ src/components/SliderPost/SliderPost.jsx
@@ -1,9 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./SliderPost.css";
 
 function SliderPost(props) {
     const { posts } = props;
     const sliderRef = useRef(null);
+    const [isDynamic, setIsDynamic] = useState(false);
+
+    useEffect(() => {
+        if (posts.length < 5) { // Sesuaikan dengan jumlah postingan yang sedikit
+            setIsDynamic(true);
+        } else {
+            setIsDynamic(false);
+        }
+    }, [posts.length]);
 
     const scrollLeft = () => {
         sliderRef.current.scrollBy({ left: -300, behavior: "smooth" });
@@ -19,9 +28,12 @@ function SliderPost(props) {
             <button className="slider-nav slider-nav-left" onClick={scrollLeft}>
                 &#8249;
             </button>
-            <div className="slider-container" ref={sliderRef}>
+            <div
+                className={`slider-container ${isDynamic ? "dynamic" : ""}`}
+                ref={sliderRef}
+            >
                 {posts.map((post, index) => (
-                    <div className="slider-post" key={index}>
+                    <div className={`slider-post ${isDynamic ? "dynamic" : ""}`} key={index}>
                         <img src={post.image} alt={post.title} className="slider-image"/>
                         <div className="slider-content">
                             <a href={post.url} className="slider-title-link">
